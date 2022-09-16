12008b50cdb1034feba17b461e26e5faa4d26af9
0xSzeth <0xSzeth@gmail.com>
2022-09-16 10:50:29 -0600

    index NewBunni event

--- src/mappings/BunniHub.ts
+++ src/mappings/BunniHub.ts
@@ -1,12 +1,19 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import { BunniHub, Compound, Deposit, NewBunni, OwnershipTransferred, PayProtocolFee, SetProtocolFee, Withdraw } from "../../generated/BunniHub/BunniHub"
-import { BunniToken } from "../../generated/schema"
+import { Address, BigInt } from "@graphprotocol/graph-ts";
+import { BunniHub, Compound, Deposit, NewBunni, OwnershipTransferred, PayProtocolFee, SetProtocolFee, Withdraw } from "../../generated/BunniHub/BunniHub";
+import { BunniToken } from "../../generated/schema";
+import { getBunniToken } from "../utils/entities";
 
 export function handleCompound(event: Compound): void {}
 
 export function handleDeposit(event: Deposit): void {}
 
-export function handleNewBunni(event: NewBunni): void {}
+export function handleNewBunni(event: NewBunni): void {
+  let bunniToken = getBunniToken(event.params.token);
+  bunniToken.pool = event.params.pool;
+  bunniToken.tickLower = BigInt.fromI32(event.params.tickLower);
+  bunniToken.tickUpper = BigInt.fromI32(event.params.tickUpper);
+  bunniToken.save();
+}
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 
